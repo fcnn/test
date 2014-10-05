9bd294b20d7c0f573ffb203c08d7c473c5c24dda
root <root@localhost.localdomain>
2014-10-05 23:47:11 +0800

    multi-interface support

--- epoll.cc
+++ epoll.cc
@@ -26,65 +26,70 @@ service_main(int sd)
 	char msg[16];
 	ssize_t len=1;
 
-	for ( ; len > 0 || ( len < 0 && errno == EAGAIN); ) {
-	len = recv(sd, msg, sizeof (msg), 0);
-        if (len == 0) {
-		printf("connection closed by peer\n");
-		close(sd);
-		return;
-        }
-        if (len < 0) {
-               if (errno != EINTR) {
-			if (errno != EAGAIN) {
-                        	perror("recv");
-				close(sd);
-			}
+	for ( ; len > 0 || ( len < 0 && errno == EINTR); ) {
+		len = recv(sd, msg, sizeof (msg), 0);
+	        if (len == 0) {
+			printf("connection closed by peer\n");
+			close(sd);
 			return;
-                }
-        }
-        else {
-                int sent_len = send(sd, msg, len, 0);
-		if (sent_len == -1) {
-                        perror("send");
-			if (errno == EPIPE || errno == ENOTCONN) {
-				printf("connection closed during send ...\n");
+       		 }
+       		 if (len < 0) {
+       		        if (errno != EINTR) {
+				if (errno != EAGAIN) {
+       		                 	perror("recv");
+					close(sd);
+				}
+				return;
+       		         }
+        	}
+        	else {
+               		 int sent_len = send(sd, msg, len, 0);
+			if (sent_len == -1) {
+                       		 perror("send");
+				if (errno == EPIPE || errno == ENOTCONN) {
+					printf("connection closed during send ...\n");
+				}
+				return;
 			}
-			return;
-		}
-		printf("sent %d\n", sent_len);
-        }
+			printf("sent %d\n", sent_len);
+        	}
 	}
 }
 
+struct __epoll_server {
+	int epollfd;
+	int listen_sd[8];
+	int sd_count;
+};
+
 static int
-init_epoll(int sd)
+set_epoll(struct __epoll_server *server)
 {
-	int flags = fcntl(sd, F_GETFL, 0);
-	if (fcntl(sd, F_SETFL, flags | O_NONBLOCK) == -1) {
-		perror("fcntl");
-		return -1;
-	}
 	int epollfd = epoll_create1(0);
 	if (epollfd == -1) {
 		perror("epoll_create1");
 		return -1;
 	}
-	struct epoll_event ev;
-	ev.events = EPOLLIN;
-	ev.data.fd = sd;
-	if (epoll_ctl(epollfd, EPOLL_CTL_ADD, sd, &ev) == -1) {
-		perror("epoll_ctl");
-		close(epollfd);
-		return -1;
+	for (int i = 0; i < server->sd_count; ++i) {
+		int sd = server->listen_sd[i];
+		struct epoll_event ev;
+		ev.events = EPOLLIN;
+		ev.data.fd = sd;
+		int flags = fcntl(sd, F_GETFL, 0);
+		if (fcntl(sd, F_SETFL, flags | O_NONBLOCK) == -1) {
+			perror("fcntl");
+			return -1;
+		}
+		if (epoll_ctl(epollfd, EPOLL_CTL_ADD, sd, &ev) == -1) {
+			perror("epoll_ctl");
+			close(epollfd);
+			return -1;
+		}
 	}
+	server->epollfd = epollfd;
 	return epollfd;
 }
  
-struct __epoll_server {
-	int epollfd;
-	int listen_sd;
-};
-
 static int
 init_server(int argc, char *argv[], struct __epoll_server *epoll_server)
 {
@@ -126,6 +131,9 @@ init_server(int argc, char *argv[], struct __epoll_server *epoll_server)
 
 	printf("success\n");
 
+	const int max_sd = sizeof epoll_server->listen_sd / sizeof epoll_server->listen_sd[0];
+
+	epoll_server->sd_count = 0;
 	for (addr_i = hostaddr; addr_i != 0; addr_i = addr_i->ai_next) {
 		char name[128];
 		char serv_name[64];
@@ -133,39 +141,43 @@ init_server(int argc, char *argv[], struct __epoll_server *epoll_server)
 			name, sizeof(name), serv_name, sizeof(serv_name),
 			NI_NUMERICHOST | NI_NUMERICSERV);
 		printf("binding %s/%s ... ", name, serv_name);
+		fflush(stdout);
 		if ((sd = socket(addr_i->ai_family, addr_i->ai_socktype, 0)) == -1) {
-			perror("");
+			perror("socket");
 			continue;
 		}
 
 		if (bind(sd, (struct sockaddr *)addr_i->ai_addr, addr_i->ai_addrlen) == -1) {
-			perror("");
+			perror("bind");
 			close(sd);
 			continue;
 		}
 
 		if (listen(sd, 5) == 0) {
 			printf("success\n");
-			break;
+			epoll_server->listen_sd[epoll_server->sd_count++] = sd;
+			if (epoll_server->sd_count == max_sd) {
+				break;
+			}
 		}
-
-		perror("listen");
-		close(sd);
+		else {
+			perror("listen");
+			close(sd);
+		}
 	}
 
 	freeaddrinfo(hostaddr);
 
-	if (addr_i == 0)
+	if (epoll_server->sd_count == 0)
 		return -1;
 
-	int epfd = init_epoll(sd); 
+	int epfd = set_epoll(epoll_server); 
 	if (epfd == -1) {
-		close(sd);
+		for (int i = 0; i < epoll_server->sd_count; ++i) {
+			close(epoll_server->listen_sd[i]);
+		}
 		return -1;
 	}
-
-	epoll_server->listen_sd = sd;
-	epoll_server->epollfd = epfd;
 
 	return 0;
 }
@@ -238,10 +250,15 @@ main(int argc, char *argv[])
 			break;
 		}
 		for (int i = 0; i < nfds; ++i) {
-			if (events[i].data.fd == epoll_server.listen_sd) {
+			int j = 0;
+			for ( ; j < epoll_server.sd_count; ++j ) {
+				if (events[i].data.fd == epoll_server.listen_sd[j])
+					break;
+			}
+			if (j < epoll_server.sd_count) {
 				struct sockaddr_in6 sa;
 				socklen_t slen = sizeof (sa);
-				int cd = accept(epoll_server.listen_sd, (struct sockaddr *)&sa, &slen);
+				int cd = accept(epoll_server.listen_sd[j], (struct sockaddr *)&sa, &slen);
 				if (cd != -1) {
 					peer_info(cd, (struct sockaddr *)&sa, slen);
 					struct epoll_event ev;
@@ -270,7 +287,8 @@ main(int argc, char *argv[])
 		}
 	}
 
-	close(epoll_server.listen_sd);
+	for (int i = 0; i < epoll_server.sd_count; ++i)
+		close(epoll_server.listen_sd[i]);
 	close(epoll_server.epollfd);
 
 	return 0;
